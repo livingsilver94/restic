5fabfcd5d14b08a379bd19368e23257ab40b7b81
Fabio Forni <development@redaril.me>
2024-11-23 12:30:23 +0100

    backend: rest: Reduce scope of variables in config_test.go

--- internal/backend/rest/config_test.go
+++ internal/backend/rest/config_test.go
@@ -16,89 +16,89 @@ func parseURL(s string) *url.URL {
 	return u
 }
 
-var configTests = []test.ConfigTestData[Config]{
-	{
-		S: "rest:http://localhost:1234",
-		Cfg: Config{
-			URL:         parseURL("http://localhost:1234/"),
-			Connections: 5,
-		},
-	},
-	{
-		S: "rest:http://localhost:1234/",
-		Cfg: Config{
-			URL:         parseURL("http://localhost:1234/"),
-			Connections: 5,
-		},
-	},
-	{
-		S: "rest:http+unix:///tmp/rest.socket:/my_backup_repo/",
-		Cfg: Config{
-			URL:         parseURL("http+unix:///tmp/rest.socket:/my_backup_repo/"),
-			Connections: 5,
-		},
-	},
-}
-
 func TestParseConfig(t *testing.T) {
+	configTests := []test.ConfigTestData[Config]{
+		{
+			S: "rest:http://localhost:1234",
+			Cfg: Config{
+				URL:         parseURL("http://localhost:1234/"),
+				Connections: 5,
+			},
+		},
+		{
+			S: "rest:http://localhost:1234/",
+			Cfg: Config{
+				URL:         parseURL("http://localhost:1234/"),
+				Connections: 5,
+			},
+		},
+		{
+			S: "rest:http+unix:///tmp/rest.socket:/my_backup_repo/",
+			Cfg: Config{
+				URL:         parseURL("http+unix:///tmp/rest.socket:/my_backup_repo/"),
+				Connections: 5,
+			},
+		},
+	}
+
 	test.ParseConfigTester(t, ParseConfig, configTests)
 }
 
-var passwordTests = []struct {
-	input    string
-	expected string
-}{
-	{
-		"rest:",
-		"rest:/",
-	},
-	{
-		"rest:localhost/",
-		"rest:localhost/",
-	},
-	{
-		"rest::123/",
-		"rest::123/",
-	},
-	{
-		"rest:http://",
-		"rest:http://",
-	},
-	{
-		"rest:http://hostname.foo:1234/",
-		"rest:http://hostname.foo:1234/",
-	},
-	{
-		"rest:http://user@hostname.foo:1234/",
-		"rest:http://user@hostname.foo:1234/",
-	},
-	{
-		"rest:http://user:@hostname.foo:1234/",
-		"rest:http://user:***@hostname.foo:1234/",
-	},
-	{
-		"rest:http://user:p@hostname.foo:1234/",
-		"rest:http://user:***@hostname.foo:1234/",
-	},
-	{
-		"rest:http://user:pppppaaafhhfuuwiiehhthhghhdkjaoowpprooghjjjdhhwuuhgjsjhhfdjhruuhsjsdhhfhshhsppwufhhsjjsjs@hostname.foo:1234/",
-		"rest:http://user:***@hostname.foo:1234/",
-	},
-	{
-		"rest:http://user:password@hostname",
-		"rest:http://user:***@hostname/",
-	},
-	{
-		"rest:http://user:password@:123",
-		"rest:http://user:***@:123/",
-	},
-	{
-		"rest:http://user:password@",
-		"rest:http://user:***@/",
-	},
-}
-
 func TestStripPassword(t *testing.T) {
+	passwordTests := []struct {
+		input    string
+		expected string
+	}{
+		{
+			"rest:",
+			"rest:/",
+		},
+		{
+			"rest:localhost/",
+			"rest:localhost/",
+		},
+		{
+			"rest::123/",
+			"rest::123/",
+		},
+		{
+			"rest:http://",
+			"rest:http://",
+		},
+		{
+			"rest:http://hostname.foo:1234/",
+			"rest:http://hostname.foo:1234/",
+		},
+		{
+			"rest:http://user@hostname.foo:1234/",
+			"rest:http://user@hostname.foo:1234/",
+		},
+		{
+			"rest:http://user:@hostname.foo:1234/",
+			"rest:http://user:***@hostname.foo:1234/",
+		},
+		{
+			"rest:http://user:p@hostname.foo:1234/",
+			"rest:http://user:***@hostname.foo:1234/",
+		},
+		{
+			"rest:http://user:pppppaaafhhfuuwiiehhthhghhdkjaoowpprooghjjjdhhwuuhgjsjhhfdjhruuhsjsdhhfhshhsppwufhhsjjsjs@hostname.foo:1234/",
+			"rest:http://user:***@hostname.foo:1234/",
+		},
+		{
+			"rest:http://user:password@hostname",
+			"rest:http://user:***@hostname/",
+		},
+		{
+			"rest:http://user:password@:123",
+			"rest:http://user:***@:123/",
+		},
+		{
+			"rest:http://user:password@",
+			"rest:http://user:***@/",
+		},
+	}
+
 	// Make sure that the factory uses the correct method
 	StripPassword := NewFactory().StripPassword
 
